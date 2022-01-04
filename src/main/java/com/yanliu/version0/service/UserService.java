25e6b63e532523cac74fd6afd80ff7cf132a1e1f
Yanliu <wyl199825@gmail.com>
2022-01-03 23:31:13 -0800

    server creates UserService for client

---
+++ src/main/java/com/yanliu/version0/service/UserService.java
@@ -0,0 +1,11 @@
+package com.yanliu.version0.service;
+
+import com.yanliu.version0.common.User;
+
+/**
+ * @author yanliu
+ * @create 2022-01-03-11:26 PM
+ */
+public interface UserService {
+    User getUserByUserId(Integer id);
+}
